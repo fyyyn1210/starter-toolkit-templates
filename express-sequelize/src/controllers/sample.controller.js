ed687d7e484b449e2efcde47959e87874104d312
fyyyn1210 <riosraskaa@gmail.com>
2025-09-14 17:06:13 +0700

    ( Fix ) : fix templates coices

--- express-sequelize/src/controllers/sample.controller.js
+++
@@ -1,9 +0,0 @@
-import { body, validationResult, query, param } from 'express-validator';
-import { Op } from 'sequelize';
-import logger from '#src/configs/winston.config.js';
-
-const SampleController = {
-
-};
-
-export default SampleController;
--- express-sequelize/src/routes/main.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import MainController from '../controllers/main.controller.js';
-
-const router = Router()
-
-// router.use(() => {}); // General middleware
-// router.get('/', MainController.index);
-
-export default router;
